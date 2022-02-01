e243d68d2fff704baa17b8ecd30a7c4605e0a111
Nutifafa Attor <attorfafa@gmail.com>
2022-02-01 13:56:36 +0000

    change background headere on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
