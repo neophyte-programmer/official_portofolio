2725991f4924bf76e238c6fa2bc219d82c0938c9
Nutifafa Attor <attorfafa@gmail.com>
2022-02-04 15:38:25 +0000

    skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,27 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll(".services__modal"), 
+    modalBtns = document.querySelectorAll(".services__button"),
+    modalClose = document.querySelectorAll(".services__modal-close")
 
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add("active-modal")
+} 
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener("click", () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener("click", () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove("active-modal")
+        })
+    })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
