72f5f5a95066b9bf7eac117b0b67f556c72b5b39
Nutifafa Attor <attorfafa@gmail.com>
2022-02-04 17:53:00 +0000

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,10 +30,24 @@ modalClose.forEach((mc) => {
 })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup(".work__container", {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Link active work */ 
+const linkWork = document.querySelectorAll(".work__item")
 
+function activeWork() {
+    linkWork.forEach(l => l.classList.remove("active-work"))
+    this.classList.add("active-work")
+}
+
+linkWork.forEach(l => l.addEventListener("click", activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
