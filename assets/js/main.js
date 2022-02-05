724b41473071bcb2bf9505b5e5d5bd6acff2cdae
Nutifafa Attor <attorfafa@gmail.com>
2022-02-05 10:44:12 +0000

    testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,78 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader(){
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+function scrollHeader() {
+	const header = document.getElementById('header')
+	// When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+	if (this.scrollY >= 50) header.classList.add('scroll-header')
+	else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
-const modalViews = document.querySelectorAll(".services__modal"), 
-    modalBtns = document.querySelectorAll(".services__button"),
-    modalClose = document.querySelectorAll(".services__modal-close")
+const modalViews = document.querySelectorAll('.services__modal'),
+	modalBtns = document.querySelectorAll('.services__button'),
+	modalClose = document.querySelectorAll('.services__modal-close')
 
 let modal = function (modalClick) {
-    modalViews[modalClick].classList.add("active-modal")
-} 
+	modalViews[modalClick].classList.add('active-modal')
+}
 
 modalBtns.forEach((mb, i) => {
-    mb.addEventListener("click", () => {
-        modal(i)
-    })
+	mb.addEventListener('click', () => {
+		modal(i)
+	})
 })
 
 modalClose.forEach((mc) => {
-    mc.addEventListener("click", () => {
-        modalViews.forEach((mv) => {
-            mv.classList.remove("active-modal")
-        })
-    })
+	mc.addEventListener('click', () => {
+		modalViews.forEach((mv) => {
+			mv.classList.remove('active-modal')
+		})
+	})
 })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-let mixerPortfolio = mixitup(".work__container", {
-    selectors: {
-        target: '.work__card'
-    },
-    animation: {
-        duration: 300
-    }
-});
+let mixerPortfolio = mixitup('.work__container', {
+	selectors: {
+		target: '.work__card',
+	},
+	animation: {
+		duration: 300,
+	},
+})
 
-/* Link active work */ 
-const linkWork = document.querySelectorAll(".work__item")
+/* Link active work */
+const linkWork = document.querySelectorAll('.work__item')
 
 function activeWork() {
-    linkWork.forEach(l => l.classList.remove("active-work"))
-    this.classList.add("active-work")
+	linkWork.forEach((l) => l.classList.remove('active-work'))
+	this.classList.add('active-work')
 }
 
-linkWork.forEach(l => l.addEventListener("click", activeWork))
+linkWork.forEach((l) => l.addEventListener('click', activeWork))
 
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    spaceBetween: 24,
+    loop: true,
+    grabCursor: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+    },
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
 
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 48,
+        },
+      },
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
-/*=============== LIGHT DARK THEME ===============*/ 
-
+/*=============== LIGHT DARK THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
